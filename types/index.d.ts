a6a07e395414dccf791392efa09bfb1ca879b37a
Ryan Carniato <ryansolid@gmail.com>
2019-04-22 18:17:38 -0700

    Better types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,9 +3,9 @@ export interface IDom {
   content ? : string;
   voidElement: boolean;
   name: string;
-  attrs: {};
+  attrs: { [key: string]: any };
   children: IDom[];
 }
 
-export declare function parse(html: string): Array<any>;
-export declare function stringify(doc: IDom): string;
+export declare function parse(html: string): IDom[];
+export declare function stringify(doc: IDom[]): string;
